0d9bb2b0ab58efa637fe5230934e6ff225db2858
Daniel Shorten <danielshorten@gmail.com>
2013-04-03 17:09:02 +0300

    Added Drug model

--- omod/src/main/webapp/resources/js/model/drug.js
+++ omod/src/main/webapp/resources/js/model/drug.js
@@ -1,20 +1,46 @@
 define(
 [
-	openhmis.url.backboneBase + 'js/model/generic'
+	openhmis.url.backboneBase + 'js/model/generic',
+	openhmis.url.backboneBase + 'js/lib/i18n'
 ],
-function(openhmis) {
+function(openhmis, __) {
+	openhmis.Drug = openhmis.GenericModel.extend({
+		meta: {
+			name: "Drug",
+			restUrl: "v1/drug"
+		},
+		
+		schema: {
+			name: { type: "Text" },
+			description: { type: "Text" },
+			dosageForm: { type: "Object" },
+			doseStrength: { type: "Number" },
+			maximumDailyDose: { type: "Number" },
+			minimumDailyDose: { type: "Number" },
+			units: { type: "Text" },
+			concept: { type: "Object" },
+			combination: { type: "Checkbox" },
+			route: { type: "Object" }
+		}
+	});
+	
 	openhmis.DrugOrder = openhmis.GenericModel.extend({
 		meta: {
 			name: "Drug Order"
 		},
 		
 		schema: {
-			drug: { type: "Text" },
+			drug: { type: "NestedModel", model: openhmis.Drug, objRef: true },
 			daw: { type: "Checkbox", title: "DAW" },
 			frequency: { type: "Text" },
 			prn: { type: "Checkbox", title: "PRN" },
 			duration: { type: "Text" },
 			instructions: { type: "TextArea"}
+		},
+		
+		validate: function(attrs) {
+			if (!attrs.drug || !attrs.drug.id) return { drug: __("Please choose a drug") };
+			return null;
 		}
 	});
 });
